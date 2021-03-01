30b098fc8b8f6c85cb093d15eafa84372c71bf8e
Victor Bello <victor.bello@voxage.com.br>
2021-03-01 20:05:09 -0300

    Finalização do primeiro projeto de estudo.
    Entendendo conceitos basicos do react como JSX e Babel.

--- README.md
+++ README.md
@@ -14,3 +14,20 @@ Outra maneira mais antiga de se criar um react-app, é adicionar o modulo create
 - create-react-app my-app
 
 Ambas as formas são equivalentes, porém a primeira foi disponibilizada apenas em versões mais recentes do node.
+
+### Babel
+
+Babel é uma ferramenta responsável por compatibilizar códigos em novas versões javascript para versões mais antigas.
+Muitos browsers não acompanham a evolução da linguagem e o código desenvolvido pode acabar gerando erros em produção, ou seja, um codigo escrito em ES6 ou ES2020, pode acabar não sendo compativel com uma versão de browser, pois ele ainda está dando suporte apenas ao ES5.
+
+Com o Babel, podemos escrever código javascript utilizando todo o poder da lingaugem e, ao "compilar" esse código é convertido para a versão ES5, podendo rodar em qualquer browser com total compatibilidade.
+
+### Projetos
+
+#### JSX
+
+Projeto inicial que mostra como criar uma simples aplicação react
+
+#### Components
+
+Projeto que visa demonstrar como utilizar a componentização no react.
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Create a react component
+const App2 = function () {
+  return <div>Hi there!</div>;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//App2 é equivalente a APP
+const App = () => {
+  const buttonText = { text: "Click me" };
+  const labelText = "Enter name:";
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
