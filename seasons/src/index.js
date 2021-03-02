606685b87d5407d19cc37f7637c5b32e7cf7b67a
Victor Bello <victor.bello@voxage.com.br>
2021-03-01 22:03:52 -0300

    Functional Components to Class Components

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,13 +6,14 @@ if (module.hot) {
   module.hot.accept();
 }
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-
-  return <div>Latitude:</div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
