5d41a9fd5c747ba89a87a76893083fadb4ffe56f
Victor Bello <victor.bello@voxage.com.br>
2021-03-01 21:12:50 -0300

    Criando componentes reutilizaveis
    Desta forma, eh possivel enviar um componente pelo props como se fosse filho de outro componente

--- components/src/index.jsx
+++ components/src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 if (module.hot) {
   module.hot.accept();
@@ -9,9 +11,36 @@ if (module.hot) {
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+        </div>
+        Are you sure you want to do this?
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          comment="Nice blog post!"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:05PM"
+          comment="Terrible post!"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 8:45AM"
+          comment="Not that good!"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
