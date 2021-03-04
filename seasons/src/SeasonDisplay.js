7732e2d344da2d6e2dc74afc9f702bf3e5eb1ad5
Victor Bello <victor.bello@voxage.com.br>
2021-03-03 22:18:14 -0300

    Adicionando icone
    `${icon} icon`} Utilizando "`" para concaternar variavel com texto

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -10,11 +10,15 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
+
   const text =
     season === "winter" ? "Burr, it is chilly" : "Let's hit the beach";
+  const icon = season === "winter" ? "snowflake" : "sun";
   return (
     <div>
+      <i className={`${icon} icon`} />
       <h1>{text}</h1>
+      <i className={`${icon} icon`} />
     </div>
   );
 };
