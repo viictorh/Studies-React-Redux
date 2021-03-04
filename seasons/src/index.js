e126e37f5875598b168d734cf41830d0bf8dcdd9
Victor Bello <victor.bello@voxage.com.br>
2021-03-03 22:13:24 -0300

    Usando ternario

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -22,7 +22,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div> Latitude: {this.state.lat} </div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading!</div>;
