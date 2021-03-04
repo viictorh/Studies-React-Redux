b07f8da13d3edf562fcaa07a8a7d2ff82d640f82
Victor Bello <victor.bello@voxage.com.br>
2021-03-04 20:08:46 -0300

    Evitando que o formulario seja enviado ao pressionar enter em um input

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,21 +1,25 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    event.target.value;
+  state = {
+    term: "",
+  };
+
+  onFormSubmit(event) {
+    event.preventDefault();
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
-            {/*             
-            O item abaixo é equivalente a chamar uma função diretamente, como acima.
-            <input type="text" onChange={e => console.log(e.target.value)}/>
-             */}
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
