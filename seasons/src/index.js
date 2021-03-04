dbe44b65cf64c581dcaf8c1be7e73246be1f048e
Victor Bello <victor.bello@voxage.com.br>
2021-03-03 21:52:34 -0300

    Refatorando contrutor
    removendo construtor e adicionando definição do state no corpo da classe diretamente.
    Isso é possivel pois o Babel ao "compilar" o código, transforma no padrao valido utilizando construtor

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,12 +7,7 @@ if (module.hot) {
 }
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    //UNICO MOMENTO QUE SE FAZ A ATRIBUIÇÃO DIRETA DO VALOR NO THIS.STATE.
-    //TODOS OS OUTROS CASOS DEVEM UTILIZAR O MÉTODO setState
-    this.state = { lat: null, errorMessage: "" };
-  }
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
