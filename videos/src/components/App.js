b664a7debfe6fbf699cf50a35d556cdc58973843
Victor Bello <victor.bello@voxage.com.br>
2021-03-05 21:31:26 -0300

    Finalizando o projeto

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import youtube from "../apis/youtube";
 import SearchBar from "./SearchBar";
+import VideoDetail from "./VideoDetail";
 import VideoList from "./VideoList";
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onFormSubmit("");
+  }
 
   onFormSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -13,7 +18,10 @@ class App extends React.Component {
       },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      selectedVideo: response.data.items[0],
+      videos: response.data.items,
+    });
   };
 
   onVideoSelect = (selectedVideo) => {
@@ -24,10 +32,19 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onFormSubmit} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ videos/src/components/VideoDetail.js
@@ -0,0 +1,25 @@
+//atalho: imr
+import React from "react";
+
+//Atalho: sfc
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <React.Fragment />;
+  }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
