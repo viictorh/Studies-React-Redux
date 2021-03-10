8dde21474e03feda74d407f8aa3129ff2976ce98
Victor Bello <victor.bello@voxage.com.br>
2021-03-10 20:04:34 -0300

    Habilitando XSS no react
    A ideia não é habilitar XSS mas a funcionalidade que permite receber um html/javascript e renderizar na tela. Essa função deve ser utilizada apenas com códigos confiaveis e evitada sempre que possivel.

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -3,13 +3,35 @@ import React, { useEffect, useState } from "react";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     const search = async () => {
-      await axios.get("https://");
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          format: "json",
+          origin: "*",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
     };
-    search();
+
+    if (term) search();
   }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -23,6 +45,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list"> {renderedResults}</div>
     </div>
   );
 };
