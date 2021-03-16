2eeb2df59f8b2aa787acadec54b162116a80ec07
Victor Bello <victor.bello@voxage.com.br>
2021-03-16 20:54:51 -0300

    Executando um ActionCreator - Redux

--- redux-songs/src/components/SongList.js
+++ redux-songs/src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
   state = {};
@@ -8,7 +9,12 @@ class SongList extends Component {
     return this.props.songs.map((song) => (
       <div key={song.title} className="item">
         <div className="right floated content">
-          <button className="ui button primary">Select</button>
+          <button
+            className="ui button primary"
+            onClick={() => this.props.selectSong(song)}
+          >
+            Select
+          </button>
         </div>
         <div className="content">{song.title}</div>
       </div>
@@ -25,4 +31,4 @@ const mapStateToProps = (state) => {
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong: selectSong })(SongList);
