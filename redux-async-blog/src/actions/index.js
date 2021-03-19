5344cfa1fa830cc471e30e83f63238f1a59f1010
Victor Bello <victor.bello@voxage.com.br>
2021-03-18 21:28:17 -0300

    Resolvendo requisicoes repetidas com a biblioteca lodash memorizando chamadas de métodos
    https://lodash.com/

--- redux-async-blog/src/actions/index.js
+++ redux-async-blog/src/actions/index.js
@@ -1,12 +1,19 @@
 //Actions creators
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+//por padrao, lodash utiliza _ como nome.
+import _ from "lodash";
 
 export const fetchPosts = () => async (dispatch /*, getState*/) => {
   const response = await jsonPlaceholder.get("/posts");
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => {
+  _fetchUser(id, dispatch);
+};
+
+/** o underscore no nome indica que eh um metodo privado e nao deve ser chamado por fora da classe */
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
