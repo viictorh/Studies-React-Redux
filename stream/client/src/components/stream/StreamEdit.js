9fd08e8dc1dacb8e3f69537c09c65113ddfd8bb6
Victor Bello <victor.bello@voxage.com.br>
2021-03-29 20:12:54 -0300

    Redux-form - Utilizando initialValues para mostrar formulário com dados pré-carregados
    O método do edit foi alterado para patch, pois ao utilizar o "PUT", normalmente atualiza-se TODOS os campos, já o métoo "PATCH", atualiza apenas os campos enviados

--- stream/client/src/components/stream/StreamEdit.js
+++ stream/client/src/components/stream/StreamEdit.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
+import _ from "lodash";
 
 class StreamEdit extends Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.stream.id, formValues);
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    return <div>streamdedit</div>;
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          onSubmit={this.onSubmit}
+          initialValues={_.pick(this.props.stream, "title", "description")}
+        />
+      </div>
+    );
   }
 }
 
@@ -19,4 +33,6 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
