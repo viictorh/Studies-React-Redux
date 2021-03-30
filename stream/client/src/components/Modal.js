b8664007de2cd43e938bea2982ef6cd80668cf88
Victor Bello <victor.bello@voxage.com.br>
2021-03-29 21:11:46 -0300

    Criando um Modal Dialog reutilizavel

--- stream/client/src/components/Modal.js
+++ stream/client/src/components/Modal.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Modal = (props) => {
+const Modal = ({ title, content, actions, onDismiss }) => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">teste</div>
+    <div onClick={onDismiss} className="ui dimmer modals visible active">
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">{title}</div>
+        <div className="content">{content}</div>
+        <div className="actions">{actions}</div>
+      </div>
     </div>,
     document.querySelector("#modal")
   );
