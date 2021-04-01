84fffc786eafb57434904458a1f4d2a6dcc1ddf1
Victor Bello <victor.bello@voxage.com.br>
2021-04-01 19:12:15 -0300

    Finalizando as navegações dos links

--- stream/client/src/components/stream/StreamShow.js
+++ stream/client/src/components/stream/StreamShow.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamShow = () => {
-  return <div>Create</div>;
+class StreamShow extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    const { stream } = this.props;
+    if (!stream) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <h1>{stream.title}</h1>
+        <h5>{stream.description}</h5>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
