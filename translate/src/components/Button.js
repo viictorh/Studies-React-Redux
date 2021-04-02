93d69d308153e0c1d8114f3f2df54e5187e232bb
Victor Bello <victor.bello@voxage.com.br>
2021-04-01 21:06:26 -0300

    Utilizando Context com default value

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends Component {
+  //o nome dessa variavel deve ser esse para funcionar o contexto
+  static contextType = LanguageContext;
+
   render() {
-    return <button className="ui button primary">Submit</button>;
+    const text = this.context === "english" ? "Submit" : "Voorleggen";
+    return <button className="ui button primary">{text}</button>;
   }
 }
 
--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Field extends Component {
+  static contextType = LanguageContext;
+
   render() {
+    const label = this.context === "english" ? "Name" : "Naam";
     return (
       <div className="ui field">
-        <label>Name</label>
+        <label>{label}</label>
         <input />
       </div>
     );
